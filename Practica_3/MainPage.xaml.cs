c966d5cb8ac09e50edc357ff8d84ce09693e133a
DonAntonio04 <berrellezantonio04@gmail.com>
2024-01-09 23:03:30 -0700

    Agregar archivos de proyecto.

---
+++ Practica_3/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica_3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Criticar(object sender, EventArgs e)
+        {
+            string nombre = Nombre.Text;
+
+            string opciones = "";
+
+            if(Alto.IsChecked)
+            {
+                opciones += "Alto,";
+            }
+             if(Feo.IsChecked)
+            {
+                opciones += "Feo,";
+            }
+             if(Listo.IsChecked)
+            {
+                opciones += "Listo,";
+            }
+             if(Extravagante.IsChecked)
+            {
+                opciones += "Extravagante,";
+            }
+             if(Raro.IsChecked)
+            {
+                opciones += "Raro,";
+            }
+             if (Grande.IsChecked)
+            {
+                opciones += "Grande,";
+            }
+          
+            string mensaje = $"{nombre} es  {opciones}";
+            Resultado.Text = mensaje;
+        }
+    }
+}
